e6c172a8be27c78053875cdeec954acf0cff4c6a
Abbas463 <abbashilal463@gmail.com>
2026-02-19 10:55:47 +0430

    Working

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ Base = declarative_base()
 class person(Base):
     __tablename__ = 'people'
     ssn = Column("ssn", Integer, primary_key=True)
+    firstname = Column("firstname", String)
+    lastname = Column("lastname", String)
+    gender = Column("gender", CHAR)
+    age = Column("age", Integer)
+
+    def __init__(self, ssn, first, last, gender, age):
+        self.ssn = ssn
+        self.firstname = first
+        self.lastname = last
+        self.gender = gender
+        self.age = age
