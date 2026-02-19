fa6370652ea63e7d2595e8fff9404bb24ce4e044
Abbas463 <abbashilal463@gmail.com>
2026-02-19 10:52:02 +0430

    Working

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class person(Base):
+    __tablename__ = 'people'
+    ssn = Column("ssn", Integer, primary_key=True)
